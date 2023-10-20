649237a99ccc946b504f5158b743637878b1281e
dev-rahim <rahimuddin5915@gmail.com>
2023-10-21 00:46:52 +0600

    premitive types in typescript

---
+++ index.js
@@ -0,0 +1,11 @@
+// __ Primitives types in typescripts
+// & string type
+var userName = "Rahim Uddin";
+// & number type
+var age = 125;
+// & bool type
+var isAdult = true;
+// & null type
+var a = null;
+// & undefined type
+var b;
---
+++ index.ts
@@ -0,0 +1,17 @@
+// __ Primitives types in typescripts
+
+// & string type
+const userName: string = "Rahim Uddin";
+
+// & number type
+const age: number = 125;
+
+// & bool type
+const isAdult: boolean = true;
+
+// & null type
+const a: null = null;
+
+// & undefined type
+let b: undefined;
+
